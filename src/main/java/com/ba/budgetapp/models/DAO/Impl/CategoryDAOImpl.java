8a567408b6893d0225062380cdb685a929ccf3c6
= <=>
2026-06-07 00:26:15 +0100

    dev dao implementation for categories

--- src/main/java/com/ba/budgetapp/models/DAO/Impl/CategoryDAOImpl.java
+++ src/main/java/com/ba/budgetapp/models/DAO/Impl/CategoryDAOImpl.java
@@ -1,34 +1,210 @@
 package com.ba.budgetapp.models.DAO.Impl;
 
-import com.ba.budgetapp.models.DAO.Interface.CrudDAO;
+
+import com.ba.budgetapp.config.DatabaseConnection;
+import com.ba.budgetapp.models.DAO.BaseDAO;
+import com.ba.budgetapp.models.DAO.Interface.CategoryDAO;
 import com.ba.budgetapp.models.entities.Category;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class CategoryDAOImpl implements CrudDAO<Category, Long> {
+
+/**
+ * Implémentation JDBC du DAO Category.
+ *
+ * @author Etudiant
+ */
+public class CategoryDAOImpl extends BaseDAO implements CategoryDAO {
+
+    private static final String INSERT =
+            """
+            INSERT INTO categories(category_name,user_id)
+            VALUES(?,?)
+            """;
+
+    private static final String FIND_BY_ID =
+            """
+            SELECT *
+            FROM categories
+            WHERE category_id = ?
+            """;
+
+    private static final String FIND_ALL =
+            """
+            SELECT *
+            FROM categories
+            ORDER BY category_name
+            """;
+
+    private static final String UPDATE =
+            """
+            UPDATE categories
+            SET category_name = ?
+            WHERE category_id = ?
+            """;
+
+    private static final String DELETE =
+            """
+            DELETE FROM categories
+            WHERE category_id = ?
+            """;
+
     @Override
-    public boolean create(Category entity) {
+    public boolean create(Category category) {
+
+        try (
+                Connection connection = getConnection();
+
+                PreparedStatement ps =
+                        connection.prepareStatement(INSERT)
+        ) {
+
+            ps.setString(
+                    1,
+                    category.getCategoryName());
+
+            ps.setLong(
+                    2,
+                    category.getUserId());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
         return false;
     }
 
     @Override
-    public Optional<Category> findById(Long aLong) {
+    public Optional<Category> findById(Long id) {
+
+        try (
+                Connection connection =
+                        DatabaseConnection.getConnection();
+
+                PreparedStatement ps =
+                        connection.prepareStatement(FIND_BY_ID)
+        ) {
+
+            ps.setLong(1, id);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                return Optional.of(mapRow(rs));
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
         return Optional.empty();
     }
 
     @Override
     public List<Category> findAll() {
-        return List.of();
+
+        List<Category> categories = new ArrayList<>();
+
+        try (
+                Connection connection =
+                        DatabaseConnection.getConnection();
+
+                PreparedStatement ps =
+                        connection.prepareStatement(FIND_ALL);
+
+                ResultSet rs = ps.executeQuery()
+        ) {
+
+            while (rs.next()) {
+
+                categories.add(mapRow(rs));
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
+        return categories;
     }
 
     @Override
-    public boolean update(Category entity) {
+    public boolean update(Category category) {
+
+        try (
+                Connection connection =
+                        DatabaseConnection.getConnection();
+
+                PreparedStatement ps =
+                        connection.prepareStatement(UPDATE)
+        ) {
+
+            ps.setString(
+                    1,
+                    category.getCategoryName());
+
+            ps.setLong(
+                    2,
+                    category.getCategoryId());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
         return false;
     }
 
     @Override
-    public boolean delete(Long aLong) {
+    public boolean delete(Long id) {
+
+        try (
+                Connection connection =
+                        DatabaseConnection.getConnection();
+
+                PreparedStatement ps =
+                        connection.prepareStatement(DELETE)
+        ) {
+
+            ps.setLong(1, id);
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
         return false;
+    }
+
+    /**
+     * Convertit une ligne SQL en objet Category.
+     */
+    private Category mapRow(ResultSet rs)
+            throws SQLException {
+
+        Category category = new Category();
+
+        category.setCategoryId(
+                rs.getLong("category_id"));
+
+        category.setCategoryName(
+                rs.getString("category_name"));
+
+        category.setUserId(
+                rs.getLong("user_id"));
+
+        return category;
     }
 }
