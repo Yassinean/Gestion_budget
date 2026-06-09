5478117f6a2460fd55f6c8a361f06a23a4a250ce
= <=>
2026-06-10 00:31:21 +0100

    update transaction dao / impl

--- src/main/java/com/ba/budgetapp/models/DAO/Interface/TransactionDAO.java
+++ src/main/java/com/ba/budgetapp/models/DAO/Interface/TransactionDAO.java
@@ -5,14 +5,18 @@ import com.ba.budgetapp.models.entities.Transaction;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 public interface TransactionDAO extends CrudDAO<Transaction, Long> {
 
-    BigDecimal getTotalIncome();
-    BigDecimal getTotalExpense();
-    BigDecimal getCurrentBalance();
-    long countTransactions();
+    BigDecimal getTotalIncome(Long userId);
+    BigDecimal getTotalExpense(Long userId);
+    BigDecimal getCurrentBalance(Long userId);
+    long countTransactions(Long userId);
     List<Transaction> findByCategory(Long categoryId);
     List<Transaction> findByDateRange(LocalDate start, LocalDate end);
     List<Transaction> search(String keyword);
+    Map<String, Double> getExpensesByCategory(Long userId);
+    Map<String, Double> getMonthlyIncome(Long userId);
+    Map<String, Double> getMonthlyExpense(Long userId);
 }
